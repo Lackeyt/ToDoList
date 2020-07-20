d926176d0a54cc2c6e491d8f874eeefa81a652ae
Tyson Lackey <lackeyt90@gmail.com>
2020-07-20 13:14:33 -0700

    update project directories and files for use with MVC
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,40 +1,20 @@
-using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList.Models
+namespace ToDoList
 {
-  class Program
+  public class Program
   {
-    static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do List");
-      Program.AddViewLogic();
-    }
-    static void AddViewLogic()
-    {
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string addView = Console.ReadLine();
-      if (addView == "Add" || addView == "add")
-      {
-        Console.WriteLine("Please enter the description for the new item.");
-        string description = Console.ReadLine();
-        Item newItem = new Item(description);
-        Console.WriteLine($"{description} has been added to your list.");
-        Program.AddViewLogic();
-      }
-      else if (addView == "View" || addView == "view")
-      {
-        Console.WriteLine("Your To Do List:");
-        foreach(Item element in Item.GetAll())
-        {
-          Console.WriteLine($"{element.Description}");
-        }
-        Program.AddViewLogic();
-      }
-      else
-      {
-        Console.WriteLine("-invalid command-");
-        Program.AddViewLogic();
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
